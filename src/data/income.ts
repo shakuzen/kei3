ff71b893cfa8135865c1158663a5e0fb51a17aba
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2025-07-04 22:51:55 +0900

    Update the median income for 2024
    The 2024 report is now available for the 国民生活基礎調査.

--- src/data/income.ts
+++ src/data/income.ts
@@ -1,2 +1,3 @@
-/** Source: https://www.mhlw.go.jp/toukei/saikin/hw/k-tyosa/k-tyosa23/index.html */
-export const MEDIAN_INCOME_VALUE = 4050000;
+/** Source: 2024（令和６）年　国民生活基礎調査の概況
+ * 概況 PDF at https://www.mhlw.go.jp/toukei/saikin/hw/k-tyosa/k-tyosa24/index.html */
+export const MEDIAN_INCOME_VALUE = 4100000;
