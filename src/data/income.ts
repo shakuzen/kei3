90f87c1911a3aa6a94660a194cb2fd809a75081f
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2025-07-04 22:56:02 +0900

    Fix linter error about whitespace

--- src/data/income.ts
+++ src/data/income.ts
@@ -1,3 +1,3 @@
-/** Source: 2024（令和６）年　国民生活基礎調査の概況
+/** Source: 2024（令和６）年 国民生活基礎調査の概況
  * 概況 PDF at https://www.mhlw.go.jp/toukei/saikin/hw/k-tyosa/k-tyosa24/index.html */
 export const MEDIAN_INCOME_VALUE = 4100000;
